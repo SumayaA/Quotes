bca5ea2a82bfd8e0cb6db513488221cafa145320
sara <sara.ahumidi4@gmail.com>
2023-01-02 19:47:27 +0300

    Initial Commit

---
+++ Quotes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Quotes
+//
+//  Created by Sara Alhumidi on 09/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Sara Alhumidi on 09/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
